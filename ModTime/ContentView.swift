d6e10318d074bb488300d95c2bd09e0c5527756a
Abhijith PM <pmabi1993@outlook.com>
2024-09-22 22:47:04 +0800

    Initial Commit

---
+++ ModTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ModTime
+//
+//  Created by Abhijith Pm on 22/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
