d444023042de882ed1fabaddebb2ede03572fc59
Abhijith PM <pmabi1993@outlook.com>
2024-09-23 00:19:17 +0800

    Initial commit

---
+++ ModTime/ModTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModTimeApp.swift
+//  ModTime
+//
+//  Created by Abhijith Pm on 22/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ModTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
